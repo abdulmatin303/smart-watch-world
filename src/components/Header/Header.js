c673a81c8c0914a7c280f036013c945365683b40
amr303 <amr.abdulmatin@gmail.com>
2022-03-26 17:38:56 +0600

    productsData added, blank component create

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header mt-4 mb-4'>
+            <h1>Smart Watch World</h1>
+        </div>
+    );
+};
+
+export default Header;
